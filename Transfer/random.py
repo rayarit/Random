bd7d6b6abb6287dcae53d2115d89be9711f91a03
ARITRA ROY <117136434+rayarit@users.noreply.github.com>
2025-01-15 01:07:03 +0530

    Update random.py

--- Transfer/random.py
+++ Transfer/random.py
@@ -1,4 +1,66 @@
 
+"""
+Created on Tuesday 02 April 2024
+@author: Azad Md Abulkalam
+@location: ISB, NTNU
+
+To run the proposed models as examples.
+"""
+
+from utils.utils_ import paint_vid, add_text_to_frames
+from utils.trainer import getTrainParams
+from utils import evaluate
+import numpy as np
+import mediapy as media
+import pickle
+from model.net import EchoTracker
+
+if __name__ == '__main__':
+    model = EchoTracker(device_ids=[0])
+    model.load(path="model/weights/echotracker", eval=True)
+    getTrainParams(model.model)
+    
+    # Specify the file path for the pickle file
+    data = "data/samples.pkl"
+
+    # Open the pickle file in binary read mode
+    with open(data, 'rb') as f:
+        # Load the contents of the pickle file into a dictionary
+        ds_list = pickle.load(f)
+
+    frames, trajs_g, visibs_g = ds_list['frames'][0], ds_list['trajs'][0], ds_list['visibility'][0]
+    points_0 = trajs_g[:, :, 0]  # taking points at frame 0
+
+    # Generate ground-truth frames
+    gt_frames = paint_vid(frames=frames.squeeze().numpy(), points=trajs_g.squeeze().numpy(), visibs=visibs_g.squeeze().numpy(), gray=True)
+    
+    # Run model inference
+    trajs_e = model.infer(frames, points_0)
+    
+    # Generate predicted frames
+    pd_frames = paint_vid(frames=frames.squeeze().numpy(), points=trajs_e.squeeze().numpy(), visibs=visibs_g.squeeze().numpy(), gray=True)
+    
+    # Add text labels
+    gt_frames = add_text_to_frames(gt_frames, "Ground-truth", color=(0, 255, 0))
+    pd_frames = add_text_to_frames(pd_frames, "Estimation")
+    
+    # Concatenate ground-truth and predicted frames side by side
+    cat_frames = np.concatenate((gt_frames, pd_frames), axis=2)
+    
+    # Save the video (no display)
+    media.write_video("results/output.mp4", cat_frames, fps=20)
+    print(f'The output video is saved at "results/output.mp4"')
+
+    '''Evaluation'''
+    metrics = evaluate.compute_metrics(points_0.numpy(), trajs_g.numpy(), visibs_g.numpy(), trajs_e.numpy(), visibs_g.numpy())
+
+    # Print evaluation metrics
+    for k, v in metrics.items():
+        print(f"{k}: {v:0.2}")
+
+
+
+==============================================
 import numpy as np
 import cv2
 import mmcv
